c8cfe0e7e70f7999b05124bb175b55e935a670a6
thugperfect <meenamani9444@gmail.com>
2023-10-12 17:17:17 +0530

    body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AnimateDirective } from './customDirective/animate.directive';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    AnimateDirective
+    AnimateDirective,
+    BodyComponent
   ],
   imports: [
     BrowserModule
