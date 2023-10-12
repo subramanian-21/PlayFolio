d95d859e9f455d2cb449a2a633426800f1655f54
thugperfect <meenamani9444@gmail.com>
2023-10-12 17:14:07 +0530

    nav styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AnimateDirective } from './customDirective/animate.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AnimateDirective
   ],
   imports: [
     BrowserModule
