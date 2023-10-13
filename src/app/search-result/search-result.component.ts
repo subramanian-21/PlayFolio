c5e9d4a1732114c4af8f865a38a6c108b5a0e687
thugperfect <meenamani9444@gmail.com>
2023-10-13 21:19:03 +0530

    search result

--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-search-result',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent {
+  result:any[] = []
+  loading:boolean = true
+@Input() searchInput:string = ""
 
+ngDoCheck(){
+  this.onSearchInput(this.searchInput)
+}
+onSearchInput(searchInput:string){
+  axios
+  .get(
+    `https://api.rawg.io/api/games?key=faf03104a734484db6f8cdc297d6cb53&search=${searchInput}`
+  )
+  .then((r) => {
+   
+    this.result = r.data.results;
+    setTimeout(() => {
+      this.loading = false
+    }, 3000);
+ 
+  console.log(r.data)
+  });
+}
 }
