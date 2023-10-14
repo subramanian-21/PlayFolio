b6b985ceec8e4cc000f82510f7bdb2630018802c
thugperfect <meenamani9444@gmail.com>
2023-10-14 15:57:05 +0530

    getting search result from api

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-navbar',
@@ -9,5 +10,23 @@ export class NavbarComponent {
 search:string = ''
 load(e:any){
   this.search = e.target.value
+  this.onSearchInput(this.search)
+}
+result:any[] = []
+loading:boolean = true
+
+onSearchInput(searchInput:string){
+  axios
+  .get(
+    `https://api.rawg.io/api/games?key=faf03104a734484db6f8cdc297d6cb53&search=${searchInput}`
+  )
+  .then((r) => {
+   
+    this.result = r.data.results;
+    setTimeout(() => {
+      this.loading = false
+    }, 3000);
+  console.log(r.data)
+  });
 }
 }
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -7,26 +7,9 @@ import axios from 'axios';
   styleUrls: ['./search-result.component.css']
 })
 export class SearchResultComponent {
-  result:any[] = []
+
   loading:boolean = true
 @Input() searchInput:string = ""
 
-ngDoCheck(){
-  this.onSearchInput(this.searchInput)
-}
-onSearchInput(searchInput:string){
-  axios
-  .get(
-    `https://api.rawg.io/api/games?key=faf03104a734484db6f8cdc297d6cb53&search=${searchInput}`
-  )
-  .then((r) => {
-   
-    this.result = r.data.results;
-    setTimeout(() => {
-      this.loading = false
-    }, 3000);
- 
-  console.log(r.data)
-  });
-}
+@Input() result:any[] =[]
 }
